a88a3a9f1783f6b7aa862546ac6eb32668463e4e
Hakim El Hattab <hakim.elhattab@gmail.com>
2012-08-05 15:49:51 -0400

    menu contents

--- js/meny.js
+++ js/meny.js
@@ -9,10 +9,11 @@
 
 	var activateX = 40,
 		deactivateX = 300,
+		startTouchX = 0,
 		isActive = false;
 
 	window.addEventListener( 'mousemove', onMouseMove, false );
-	window.addEventListener( 'touchmove', onTouchMove, false );
+	window.addEventListener( 'touchstart', onTouchStart, false );
 
 	function onMouseMove( event ) {
 		var x = event.clientX,
@@ -26,18 +27,45 @@
 		}
 	}
 
-	function onTouchMove( event ) {
+	function onTouchStart( event ) {
+		lastTouchX = event.touches[0].clientX;
 
+		if( isActive || lastTouchX < activateX ) {
+			event.preventDefault();
+
+			window.addEventListener( 'touchmove', onTouchMove, false );
+			window.addEventListener( 'touchend', onTouchEnd, false );
+		}
+	}
+
+	function onTouchMove( event ) {
+		var x = event.touches[0].clientX;
+
+		if( isActive && x < lastTouchX - activateX ) {
+			deactivate();
+		}
+		else if( !isActive && lastTouchX < activateX && x > lastTouchX + activateX ) {
+			activate();
+		}
+	}
+	
+	function onTouchEnd( event ) {
+		window.addEventListener( 'touchmove', onTouchMove, false );
+		window.addEventListener( 'touchend', onTouchEnd, false );
 	}
 
 	function activate() {
-		isActive = true;
-		document.documentElement.classList.add( 'meny-active' );
+		if( isActive === false ) {
+			isActive = true;
+			document.documentElement.classList.add( 'meny-active' );
+		}
 	}
 
 	function deactivate() {
-		isActive = false;
-		document.documentElement.classList.remove( 'meny-active' );
+		if( isActive === true ) {
+			isActive = false;
+			document.documentElement.classList.remove( 'meny-active' );
+		}
 	}
 
 })();
